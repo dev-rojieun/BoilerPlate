0ed5abb661a3d01bf3b5a221c48c5e5962517f0b
dev-rojieun <dev.rojieun@gmail.com>
2021-01-20 13:47:13 +0900

    비밀정보 보호

--- Models/User.js
+++ Models/User.js
@@ -5,38 +5,41 @@ const userSchema = mongoose.Schema({
 
     // 필드 작성하기
     name : {
-        type : String,
+        type: String,
         maxlength : 50
     },
     email : {
-        type : String,
+        type: String,
         trim : true, // trim 이 뭐냐면, 혹시 모를 사용자의 실수로 스페이스가 있다면 없애주는 역할 
         unique : 1 // 유일값
     },
     password : {
-        type : String,
+        type: String,
         minlength : 5
     },
     lastname : {
-        type : String,
+        type: String,
         maxlength : 50
     },
     role : { // 권한
         type : Number,
         default : 0 // 기본권한 0
     },
-    image : String,
+    image : {
+        type: String,
+    },
     token : {  // 유효성관리
-        type : String 
+        type: String,
     },
     tokenExp : { // 토큰 유효기간
-        type : String
+        type : Number
     }
-    
+
 });
 
 
-const User = mongoose.model('User', 'userSchema')    // 스키마는 모델에 감싸진다... 이렇게 모델로 감싼다.
+
+const User = mongoose.model('User', userSchema);   // 스키마는 모델에 감싸진다... 이렇게 모델로 감싼다.
              // 몽구스.모델 ( 모델명 , 스키마명 );
 
 // 이 모델을 다른 파일에서도 쓰도록 했다.
--- index.js
+++ index.js
@@ -2,38 +2,42 @@ const express = require('express');  // express 모듈을 가져온다.
 const app = express();  // 이 함수를 이용해서 새로운 App 을 만든다.
 const port = 5000;   // 5000 번 포트를 백서버로 둬서 
 
-// Mongooes Module 을 이용하여 MongoDB를 연결하자.
-const mongoose = require('mongoose');
-
-// 회원가입시 필요한 정보를 얻기 위해 User model import
-const { User } = require('./Models/User');
-
 // body-parser : client 에서 오는 정보를 Server에서 분석해서 가져올 수있게 해준다.
 const bodyPaser = require('body-parser');
 
+// 회원가입시 필요한 정보(아이디, 비번, 이메일 등)를 얻기 위해 User model import
+const { User } = require('./Models/User');
+
+// 몽고DB의 정보를 가져오기 위해!
+const config = require('./config/key');
+
+// Mongooes Module 을 이용하여 MongoDB를 연결하자.
+const mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb+srv://jero:abcd1234@boilerplate.xoybt.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+
+mongoose.connect(config.mogoURI, {
     useNewUrlParser : true,
     useUnifiedTopology : true,
     useCreateIndex : true,
-    useFindAndModify : true     // 이걸 써줘야 에러가 안떠서 써줌!
+    useFindAndModify : false     // 이걸 써줘야 에러가 안떠서 써줌!
 }).then( () => console.log('MongoDB Connected ... Success') )
   .catch( err => console.log(err) )
 
 
 // express app을 넣은 후에
-app.get
-('/', (req, res) => // 루트 디렉토리 / 에 
-{
-  res.send('Hello World!') // 헬로월드를 출력되게 해준다..
-})
+app.get('/', (req, res) => // 루트 디렉토리 / 에 
+  res.send('Hello World! Greeting!') // 헬로월드를 출력되게 해준다..
+)
+
+
 
 app.use(bodyPaser.urlencoded({extended : true}));  // bodyPaser.urlencoded -> Applicaiton/x-www-form-urlencoded 로 된 정보를 분석해서 가져올 수 있게 해준다. 
 app.use(bodyPaser.json()); // Application/Json 타입을 분석해서 가져올 수 있게 해줌
 
 
+// 라우트의 엔드포인트는 register
 app.post ('/register', (req, res) => {
 
     // 회원가입시 필요한 정보(User.js ) 를 Client 에서 가져오면
@@ -52,13 +56,13 @@ app.post ('/register', (req, res) => {
     const user = new User ( req.body );
 
     // user 를 이용해서 유저모델의 정보를 저장한다. ..... .save() < 이거는 몽고DB에서 오는 메서드임 
-    user.save( (err, userInfo) => {  // 콜백 함수
+    user.save( (err, UserInfo) => {  // 콜백 함수
             // 만약 저장시 에러가 있다면, 클라이언트에 에러 정보를 json 형식으로 전달
         if(err) return res.json({ success : false , err})   
 
         //성공시  -- res.status(200) 성공했다는 표시 클라이언트에게 출력 
         return res.status(200).json({
-            success : True
+            success : true
         })
     });
 
